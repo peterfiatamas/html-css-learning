34077ac054f694a88b3576fddd8cfd672187f076
Péterfia Tamás <peterfiathomas@gmail.com>
2017-07-28 21:51:05 +0200

    ajax

---
+++ ajax.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function(){
+    $('#mehet').on('click', mehet);
+    
+});
+
+function mehet()
+{
+    $('div').load('lap.html', function(){alert('Ez egy másik oldal, melyet úgy értünk el , hogy az oldalunk nem töltött újra!');});
+    $('div').load('lap.html #tolkein-szoveg');
+}*/
